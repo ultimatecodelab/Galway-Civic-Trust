3129781f7a3818d3a8bde17a9ecdabbaf49a6516
Ultimatecodelab <adgn353@gmail.com>
2016-02-01 19:57:51 +0000

    Tour modal updated
    mongoose tour modal has been updated to sort/arrange the tours by category

--- AdminPanel/server/api/tour/tourModel.js
+++ AdminPanel/server/api/tour/tourModel.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var TourSchema = new Schema({
   image: String,
   title: String,
-  description: String,
+ /* description: String,
   location: String,
   xCoordinate: String,
   yCoordinate: String,
@@ -16,8 +16,8 @@ var TourSchema = new Schema({
     type: Schema.ObjectId,
     ref: 'User'
   },
-  email: String,
-  userName: String,
+  //email: String,
+  //userName: String,*/
   createTime: {
     type: Date,
     'default': Date.now
