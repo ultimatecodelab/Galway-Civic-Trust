7998fe134838737c9fbca4f7c023b122cea9633a
Ultimatecodelab <adgn353@gmail.com>
2016-03-01 21:50:41 +0000

    System Admin Role / Super User - portal for user management

--- AdminPanel/client/app/admin/admin.Ctrl.js
+++ AdminPanel/client/app/admin/admin.Ctrl.js
@@ -5,43 +5,14 @@
     .module('app')
     .controller('AdminCtrl', AdminCtrl);
 
-  AdminCtrl.$inject = ['$scope', 'Auth', '$modal', 'adminAPI', '$alert', 'looksAPI'];
+  AdminCtrl.$inject = ['$scope', 'Auth', '$modal', 'adminAPI', '$alert', 'toursAPI'];
 
-  function AdminCtrl($scope, Auth, $modal, adminAPI, $alert, looksAPI) {
-
-    $scope.looks = [];
+  function AdminCtrl($scope, Auth, $modal, adminAPI, $alert, toursAPI) {
     $scope.users = [];
     $scope.user = {};
-    $scope.editLook = {};
     $scope.deleteBtn = true;
-
-    var alertSuccess = $alert({
-      title: 'Saved ',
-      content: 'Look has been edited',
-      placement: 'top-right',
-      container: '#alertContainer',
-      type: 'success',
-      duration: 8
-    });
-
-    var alertFail = $alert({
-      title: 'Not Saved ',
-      content: 'Look has failed to edit',
-      placement: 'top-right',
-      container: '#alertContainer',
-      type: 'warning',
-      duration: 8
-    });
-
-    var myModal = $modal({
-      scope: $scope,
-      show: false
-    });
-
-    $scope.showModal = function() {
-      myModal.$promise.then(myModal.show);
-    }
-
+	
+	//displays all the users of the system
     adminAPI.getAllUsers()
       .then(function(data) {
         $scope.users = data.data;
@@ -50,16 +21,7 @@
         console.log('error getting users');
         console.log(err);
       });
-
-    looksAPI.getAllLooks()
-      .then(function(data) {
-        console.log(data);
-        $scope.looks = data.data;
-      })
-      .catch(function(err) {
-        console.log('failed to get all looks');
-      })
-
+	//deleting the user
     $scope.deleteUser = function(user) {
       adminAPI.deleteUser(user)
         .then(function(data) {
@@ -72,51 +34,5 @@
           console.log(err);
         });
     }
-
-    $scope.editLook = function(look) {
-      looksAPI.getUpdateLook(look)
-        .then(function(data) {
-          console.log(data);
-          $scope.editLook = data.data;
-        })
-        .catch(function(err) {
-          console.log('failed to edit look ' + err);
-        });
-    }
-
-    $scope.saveLook = function() {
-      var look = $scope.editLook;
-
-      looksAPI.updateLook(look)
-        .then(function(data) {
-          console.log('Look updated');
-          console.log(data);
-          $scope.editLook.title = '';
-          $scope.editLook.description = '';
-          alertSuccess.show();
-        })
-        .catch(function(err) {
-          console.log('failed to update' + err);
-          alertFail.show();
-        });
-    }
-
-    $scope.deleteLook = function(look) {
-      looksAPI.deleteLook(look)
-        .then(function(data) {
-          var index = $scope.looks.indexOf(look);
-          $scope.editLook.description = '';
-          $scope.editLook.title = '';
-          $scope.deleteBtn = false;
-          alertSuccess.show();
-          $scope.looks.splice(index, 1);
-          console.log('success, look deleted');
-        })
-        .catch(function(err) {
-          alertFail.show();
-          console.log('failed to delete look' + err);
-        });
-    }
-
   }
 })();
