0719d3de62ce10f1d96f491a7d496fee14bdce60
Ultimatecodelab <adgn353@gmail.com>
2016-03-14 21:35:59 +0000

    modified admin controller and html page

--- AdminPanel/client/app/account/forgotpassword/forgotpassword.controller.js
+++ AdminPanel/client/app/account/forgotpassword/forgotpassword.controller.js
@@ -7,7 +7,7 @@ angular.module('app')
     $scope.reset = function(form) {
       $scope.submitted = true;
       if(form.$valid) {
-        $http.post('http://127.0.0.1/forgotpassword/?email=' + $scope.user.email)
+        $http.post('http://127.0.0.1:9000/forgotpassword/?email=' + $scope.user.email)
         .then( function() {
           $scope.message = 'An email with your new password has been sent to your email address.';
         });
